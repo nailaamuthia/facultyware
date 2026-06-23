f234073f42925f434ce4b4b0b6ee804a624f87ac
Salmiahafifah80 <salmiahafifah80@gmail.com>
2026-06-23 13:39:05 +0700

    fix: restore approval views and fix app.js router

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var publikasiRouter = require('./routes/publikasi');
+var approvalRouter = require('./routes/approval');
 
 const { notFoundHandler, errorHandler } = require('./middlewares/error');
 
@@ -54,6 +55,7 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/publikasi', publikasiRouter);
+app.use('/approval', approvalRouter);
 
 // ─── Error Handlers ───
 app.use(notFoundHandler);
